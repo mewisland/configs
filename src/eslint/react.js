643216020926863aa02885f69477029aa5739413
vew94 <vew943@gmail.com>
2025-10-24 15:42:53 +0900

    feat(react): add eslint-plugin-react-perf

--- src/eslint/react.js
+++ src/eslint/react.js
@@ -2,6 +2,7 @@ import { defineConfig } from "eslint/config";
 
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
+import reactPerf from "eslint-plugin-react-perf";
 
 import baseConfig from "./eslint.config.js";
 
@@ -9,7 +10,8 @@ import baseConfig from "./eslint.config.js";
  * React-specific ESLint flat configuration.
  *
  * This file composes the base configuration with recommended rules from
- * `eslint-plugin-react` and `eslint-plugin-react-hooks`. It also enforces
+ * `eslint-plugin-react`, `eslint-plugin-react-hooks`,
+ * `eslint-plugin-react-perf`. It also enforces
  * the `react-hooks/exhaustive-deps` rule as an error to catch missing hook
  * dependencies during development.
  *
@@ -29,6 +31,7 @@ export default defineConfig([
 	baseConfig,
 	react.configs.flat.recommended,
 	reactHooks.configs.flat.recommended,
+	reactPerf.configs.flat.all,
 	{
 		rules: {
 			"react-hooks/exhaustive-deps": "error",
